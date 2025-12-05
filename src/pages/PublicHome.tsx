9d78cf8d1ba2cc28a2af17a43de5c9c91763d2a7
Artacalan <christian94rz@gmail.com>
2025-12-05 15:29:02 +0100

    added example of routes

---
+++ src/pages/PublicHome.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PublicHome: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Public Home Page</h1>
+            <p>This is a public area accessible to everyone.</p>
+        </div>
+    );
+};
+
+export default PublicHome;
